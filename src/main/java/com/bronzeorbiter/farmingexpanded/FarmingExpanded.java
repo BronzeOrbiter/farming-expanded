bb98f8b5fd15ac23a92bd505241399bde90b6798
BronzeOrbiter <>
2020-08-03 13:05:24 +0100

    Initial commit

---
+++ src/main/java/com/bronzeorbiter/farmingexpanded/FarmingExpanded.java
@@ -0,0 +1,26 @@
+package com.bronzeorbiter.farmingexpanded;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FarmingExpanded implements ModInitializer {
+
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static final String MOD_ID = "farmingexpanded";
+    public static final String MOD_NAME = "Farming Expanded";
+
+    @Override
+    public void onInitialize() {
+        log(Level.INFO, "Initializing");
+        //TODO: Initializer
+    }
+
+    public static void log(Level level, String message){
+        LOGGER.log(level, "["+MOD_NAME+"] " + message);
+    }
+
+}
